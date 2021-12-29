572eea0ccade991ae8a7540d44e2b9dd2ab03345
JasmineO <jasmine.ortizetienne@gmail.com>
2021-12-29 17:35:19 -0600

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -38,16 +39,38 @@ class About extends Component {
             );
         };
 
-        return(
+        if (this.props.partners.isLoading) {
+            return(
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Loading />
+                    </Card> 
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.erMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        return (
             <ScrollView>
                 <Mission />
-                <Card title="Community Partners">
-                    <FlatList 
+                <Card
+                    title='Community Partners'>
+                    <FlatList
                         data={this.props.partners.partners}
                         renderItem={renderPartner}
-                        keyExtractor={item => item.id.toString()}
+                        keyExtractor={item=>item.id.toString()}
                     />
-                </Card> 
+                </Card>
             </ScrollView>
         );
     }
